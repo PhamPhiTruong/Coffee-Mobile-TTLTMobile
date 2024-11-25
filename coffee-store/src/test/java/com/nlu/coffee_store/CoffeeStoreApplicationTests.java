9517b95c0396fd47a2ab485d6a1ed9cc71f56b5c
PhamPhiTruong <21130593@st.hcmuaf.edu.vn>
2024-11-25 20:28:04 +0700

    Add project

---
+++ coffee-store/src/test/java/com/nlu/coffee_store/CoffeeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlu.coffee_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
